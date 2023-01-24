1db6e4b541ed95f506debe878d580f6baefae6b0
Shokhjakhon <rs145609@gmail.com>
2023-01-24 15:41:23 +0500

    Added new files

---
+++ Locators.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+
+namespace Locators;
+
+public static class Locators
+{
+    public static By SignIn => By.XPath("//header[@id=\"header-content\"]//a[contains(@class,\"ssrcss-7qt8yw\")]");
+
+    public static By SignInXpathAxes =>
+        By.XPath("//header[@id=\"header-content\"]//descendant::a[contains(@class,\"7qt8yw\")]");
+
+    public static By Home =>
+        By.XPath(
+            "//header[@id=\"header-content\"]//li[contains(@class,\"15542on\")]//a[contains(@class,\"e1gviwgp17\")]");
+
+    public static By HomeXpathAxes => By.XPath(
+        "//header[@id=\"header-content\"]//descendant::li[contains(@class,\"15542on\")]//a[contains(@class,\"e1gviwgp17\")]");
+
+    public static By Logo => By.XPath("//header[@id=\"header-content\"]//div[contains(@class,\"g08l83\")]");
+
+    public static By LogoXpathAxes =>
+        By.XPath("//header[@id=\"header-content\"]//descendant::div[contains(@class,\"g08l83\")]");
+
+    public static By SearchBar =>
+        By.XPath("//header[@id=\"header-content\"]//div[contains(@aria-label,\"Search BBC\")]");
+
+    public static By SearchBarXpathAxes =>
+        By.XPath("//header[@id=\"header-content\"]//descendant::div[contains(@aria-label,\"Search BBC\")]");
+    
+}
---
+++ LocatorsTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Locators;
+
+[TestFixture, Parallelizable(ParallelScope.Children), FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class LocatorsTest
+{
+    private IWebDriver _driver = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _driver = new ChromeDriver();
+        Uri url = new Uri("https://www.bbc.com/sport");
+        _driver.Manage().Window.Maximize();
+        _driver.Navigate().GoToUrl(url);
+    }
+
+    [Test]
+    public void Task_1()
+    {
+        var signInIcon = _driver.FindElement(Locators.SignIn);
+        Assert.That(signInIcon.Displayed);
+
+        var homeIcon = _driver.FindElement(Locators.Home);
+        Assert.That(homeIcon.Displayed);
+
+        var logoIcon = _driver.FindElement(Locators.Logo);
+        Assert.That(logoIcon.Displayed);
+
+        var searchBarIcon = _driver.FindElement(Locators.SearchBar);
+        Assert.That(searchBarIcon.Displayed);
+    }
+
+    [Test]
+    public void Task_2()
+    {
+        var homeIconXpath = _driver.FindElement(Locators.HomeXpathAxes);
+        Assert.That(homeIconXpath.Displayed);
+
+        var signInIconXpath = _driver.FindElement(Locators.SignInXpathAxes);
+        Assert.That(signInIconXpath.Displayed);
+
+        var logoIconXpath = _driver.FindElement(Locators.LogoXpathAxes);
+        Assert.That(logoIconXpath.Displayed);
+
+        var searchBarIconXpath = _driver.FindElement(Locators.SearchBarXpathAxes);
+        Assert.That(searchBarIconXpath.Displayed);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _driver.Quit();
+    }
+}
