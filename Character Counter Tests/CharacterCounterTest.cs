c6215fcc276647d82d8e7c2ea5d276b29c65fd03
Shokhjakhon <rs145609@gmail.com>
2023-01-24 11:23:58 +0500

    Added new files

---
+++ Character Counter Tests/CharacterCounterTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using Unit_Tests.Character_counter;
+
+namespace Unit_Tests.Character_Counter_Tests;
+
+[TestFixture, Parallelizable(ParallelScope.Self)]
+public class CharacterCounterTest
+{
+    private CharacterCounter? _characterCounter;
+
+    [SetUp]
+    public void SetUp() => _characterCounter = new CharacterCounter();
+
+    [TestCase(null, ExpectedResult = -1)]
+    [TestCase("", ExpectedResult = -1)]
+    [TestCase("Abby", ExpectedResult = 1)]
+    [TestCase("Abby and Dean went to kindergarten.", ExpectedResult = 3)]
+    public int TestNonIdenticalConsecutiveLetters(string str)
+    {
+        _characterCounter!.ChangeArgument(str);
+        var result = _characterCounter!.ConsecutiveNonIdenticalLetter();
+        return result;
+    }
+
+
+    [TestCase("Abby", ExpectedResult = 2)]
+    [TestCase("Abby and Dean went to kindergarten.", ExpectedResult = 6)]
+    [TestCase("Grammar", ExpectedResult = 1)]
+    [TestCase("Book", ExpectedResult = 1)]
+    public int TestIdenticalConsecutiveLetters(string str)
+    {
+        _characterCounter!.ChangeArgument(str);
+        var result = _characterCounter!.ConsecutiveIdenticalLetter();
+        return result;
+    }
+
+    [TestCase("12589", ExpectedResult = 2)]
+    [TestCase("1598369457", ExpectedResult = 1)]
+    [TestCase("9574126534", ExpectedResult = 2)]
+    [TestCase("122957789", ExpectedResult = 3)]
+    public int TestIdenticalConsecutiveDigits(string str)
+    {
+        _characterCounter!.ChangeArgument(str);
+        var result = _characterCounter!.ConsecutiveNonIdenticalLetter();
+        return result;
+    }
+}
---
+++ Character Counter/CharacterCounter.cs
@@ -0,0 +1,68 @@
+﻿namespace Unit_Tests.Character_counter;
+
+public class CharacterCounter
+{
+    //Consecutive means (in this project) is the same as 10,11,12 or a,b,c , but it can not be like 12,11,10 or c,b,a
+    private string _argument;
+
+    public CharacterCounter(string argument = "") => _argument = argument;
+
+    public void ChangeArgument(string str = "") => _argument = str;
+
+    public int ConsecutiveNonIdenticalLetter()
+    {
+        if (string.IsNullOrEmpty(_argument))
+        {
+            return -1;
+        }
+
+        int result = 0;
+        string argument = Trimmer(_argument.ToLower(), ' ');
+        int length = argument.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if (i + 1 < length && (argument[i] - argument[i + 1] == -1))
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public int ConsecutiveIdenticalLetter()
+    {
+        if (string.IsNullOrEmpty(_argument))
+        {
+            return -1;
+        }
+
+        int result = 0;
+        string str = _argument.ToLower();
+        string argument = Trimmer(str, ' ');
+        int length = argument.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if (i + 1 < length && (argument[i] - argument[i + 1] == -1 || argument[i].Equals(argument[i + 1])))
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+
+    //Trimmer method returns a string without any whitespace
+    private string Trimmer(string str, char ele)
+    {
+        string result = "";
+        foreach (var s in str)
+        {
+            if (!s.Equals(ele)) result += s;
+        }
+
+
+        return result;
+    }
+}
