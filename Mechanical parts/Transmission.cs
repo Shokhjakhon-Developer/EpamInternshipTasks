132e189209ed2410bfe5d32cb5bf361dc0fc2a70
Shokhjakhon) <93704401+Shokhjakhon-Developer@users.noreply.github.com>
2023-02-11 00:58:57 +0500

    Update Transmission.cs

--- Mechanical parts/Transmission.cs
+++ Mechanical parts/Transmission.cs
@@ -8,11 +8,12 @@ public class Transmission
 
     public Transmission(string type, int numOfGears, string manufacturer)
     {
-        (Type, NumOfGears, Manufacturer) = (type, numOfGears, manufacturer);
         Type = !type.Equals("") ? type : throw new ArgumentException("Transmission type is empty string.");
         NumOfGears = numOfGears is > 0 and <= 20
             ? numOfGears
             : throw new ArgumentException($"At least 1 gear and at max 20 is expected. Actual: {numOfGears}.");
-        Type = !manufacturer.Equals("") ? manufacturer : throw new ArgumentException("Manufacturer is empty string.");
+        Manufacturer = !manufacturer.Equals("")
+            ? manufacturer
+            : throw new ArgumentException("Manufacturer is empty string.");
     }
 }
